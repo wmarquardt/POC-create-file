42db99e6be78691f82324f270357112a30620f76
William Marquardt <williammqt@gmail.com>
2021-05-15 22:30:50 -0300

    add command line arguments

--- main.go
+++ main.go
@@ -4,19 +4,25 @@ import (
 	"math/rand"
 	"time"
 	"unsafe"
+	"flag"
 )
 
 func main() {
+	outputPtr := flag.String("o", "", "output path")
+	size := flag.String("s", "", "file size")
 
-var totalSize = uintptr(104857600)
-var currentSize = uintptr(0)
+	flag.Parse()
 
-for currentSize < totalSize{
-	rand.Seed(time.Now().UTC().UnixNano())
-	c := fmt.Sprintf("%c", randInt(1, 8000))
-	fmt.Printf(c)
-	currentSize += unsafe.Sizeof(c)
- }
+	fmt.Printf("Creating file in %s with size: %s", outputPtr, size)
+	var totalSize = uintptr(104857600)
+	var currentSize = uintptr(0)
+
+	for currentSize < totalSize{
+		rand.Seed(time.Now().UTC().UnixNano())
+		c := fmt.Sprintf("%c", randInt(1, 8000))
+		fmt.Printf(c)
+		currentSize += unsafe.Sizeof(c)
+	 }
 }
 
 func randInt(min int, max int) int {
